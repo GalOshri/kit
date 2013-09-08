0cc0935bce0d9a47cbd8ab7b29f22b09364ade4f
GalOshri <galoshri@gmail.com>
2013-09-08 15:59:07 -0700

    Templates

--- kit/reminder/views.py
+++ kit/reminder/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the KIT.")
+	template = loader.get_template('reminder/index.html')
+	context = { }
+	return render(request, 'polls/index.html', context)
 	
 	
